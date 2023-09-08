716a726468909b5370eababb0bc082c5a202cf35
Jittakan <sn.jittakan@hotmail.com>
2023-09-08 09:54:39 +0700

    Remove preloader for test only

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -1,13 +1,13 @@
 // preloader.js
 
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    // Hide the preloader when all page resources are loaded
-    const header = document.querySelector("header");
-    const main = document.querySelector("main");
-    const preloader = document.querySelector("#preloader");
-    header.style.display = "block";
-    main.style.display = "block";
-    preloader.style.display = "none";
-  }, 2); // 2000 milliseconds (2 seconds)
-});
+// window.addEventListener("load", function () {
+//   setTimeout(function () {
+//     // Hide the preloader when all page resources are loaded
+//     const header = document.querySelector("header");
+//     const main = document.querySelector("main");
+//     const preloader = document.querySelector("#preloader");
+//     header.style.display = "block";
+//     main.style.display = "block";
+//     preloader.style.display = "none";
+//   }, 2); // 2000 milliseconds (2 seconds)
+// });
