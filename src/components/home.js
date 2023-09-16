62cadb594aedc23faf81b0e92b0e0a9645ae11e8
Jittakan <sn.jittakan@hotmail.com>
2023-09-16 23:21:09 +0700

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,4 @@
 const sections = document.querySelectorAll(".content-section");
-let touchStartY = null;
 
 window.addEventListener(
   "wheel",
@@ -36,44 +35,3 @@ window.addEventListener(
   },
   { passive: false }
 );
-
-// Add touch event handling
-window.addEventListener("touchstart", function (e) {
-  touchStartY = e.touches[0].clientY;
-});
-
-window.addEventListener("touchmove", function (e) {
-  if (touchStartY === null) {
-    return;
-  }
-
-  const touchEndY = e.touches[0].clientY;
-  const deltaY = touchEndY - touchStartY;
-  touchStartY = touchEndY;
-
-  let nearestSectionIndex = -1;
-  let minDistance = Number.MAX_VALUE;
-
-  sections.forEach((section, index) => {
-    const rect = section.getBoundingClientRect();
-    const distance = Math.abs(rect.top);
-
-    if (deltaY > 0 && rect.top > 0 && distance < minDistance) {
-      nearestSectionIndex = index;
-      minDistance = distance;
-    } else if (deltaY < 0 && rect.top < 0 && distance < minDistance) {
-      nearestSectionIndex = index;
-      minDistance = distance;
-    }
-  });
-
-  if (nearestSectionIndex !== -1) {
-    sections[nearestSectionIndex].scrollIntoView({
-      behavior: "smooth",
-    });
-  }
-});
-
-window.addEventListener("touchend", function () {
-  touchStartY = null;
-});
