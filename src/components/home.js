42b541e5c1f8e3fb866243b16e499b69cb8364de
Jittakan <sn.jittakan@hotmail.com>
2023-12-05 21:14:52 +0700

    UI

--- src/components/home.js
+++ src/components/home.js
@@ -18,32 +18,6 @@ const weLayer1 = document.querySelector(".we-layer-1");
 const weLayer2 = document.querySelector(".we-layer-2");
 const worldSubText = document.querySelector(".world-sub-text");
 const moreDetailA = document.querySelectorAll(".world-base-more-detail a");
-let globalIndex;
-document.addEventListener("DOMContentLoaded", function () {
-  const baseColor = "#000000";
-  const targetColor = "#343a40";
-  const world = document.getElementById("world");
-  for (let i = 1; i <= 12; i++) {
-    const circle = document.createElement("div");
-    circle.className = "circle circle-" + i;
-    circle.style.top = 70 - i * 20 + "%";
-    circle.style.width = 20 + i * 40 + "dvh";
-    circle.style.height = 20 + i * 40 + "dvh";
-    circle.style.backgroundColor = adjustColor(baseColor, targetColor, i / 10);
-    circle.style.zIndex = 8 - i;
-    world.appendChild(circle);
-  }
-  // for (let i = 1; i <= 7; i++) {
-  //   const circle = document.createElement("div");
-  //   circle.className = "circle circle-" + i;
-  //   circle.style.top = 60 - i * 20 + "%";
-  //   circle.style.width = 20 + i * 50 + "dvh";
-  //   circle.style.height = 20 + i * 50 + "dvh";
-  //   circle.style.backgroundColor = adjustColor(baseColor, targetColor, i / 10);
-  //   circle.style.zIndex = 8 - i;
-  //   world.appendChild(circle);
-  // }
-});
 
 function changeInAnimation() {
   circle_0.style.animation = "pulse-bold 1.2s ease-out infinite";
@@ -62,22 +36,6 @@ function changeOutAnimation() {
     "drop-shadow(0rem 0rem 0.2rem rgba(42, 42, 41, 0.5)) brightness(90%)";
 }
 
-function adjustColor(startColor, endColor, percentage) {
-  const start = hexToRgb(startColor);
-  const end = hexToRgb(endColor);
-  const r = Math.round(start.r + percentage * (end.r - start.r));
-  const g = Math.round(start.g + percentage * (end.g - start.g));
-  const b = Math.round(start.b + percentage * (end.b - start.b));
-  return `rgb(${r}, ${g}, ${b})`;
-}
-
-function hexToRgb(hex) {
-  return {
-    r: parseInt(hex.slice(1, 3), 16),
-    g: parseInt(hex.slice(3, 5), 16),
-    b: parseInt(hex.slice(5, 7), 16),
-  };
-}
 let isRotationAllowed = false;
 
 function rotateWorld(degrees, index) {
